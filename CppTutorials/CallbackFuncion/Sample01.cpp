f669dc747050360283fba0bc7501de81eb69aa94
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2021-11-17 10:12:00 +0330

    Update Sample01.cpp

--- CppTutorials/CallbackFuncion/Sample01.cpp
+++ CppTutorials/CallbackFuncion/Sample01.cpp
@@ -1,13 +1,64 @@
+/*************************************************************************************
+ Copyright (C) 2021 Over-Infinity.
+ Everyone is permitted to copy and distribute verbatim copies of this license document
+ This Sampel shows how to use callback function in c++11 and later version
+**************************************************************************************/
 
 #include <iostream>
-#include <functional>
+#include<functional>
 
-using namespace std;
+class BarClass;
+typedef std::function<void()> CallBack_arg0_t;
+typedef std::function<void(int)> CallBack_arg1_t;
+
+class FooClass{
+  public:
+    FooClass(CallBack_arg0_t _fun0, CallBack_arg1_t _fun1){
+      
+      callback_0 =_fun0;
+      callback_1 = _fun1;
+      num = 100;
+      
+    }
+    void Foo_do_Somthing(){
+        /* User Code Here  */
+        callback_0();
+        callback_1(num);
+    }
+    
+  private: 
+    CallBack_arg0_t callback_0;
+    CallBack_arg1_t callback_1;
+    int num;
+};
+
+class BarClass{
+    public:
+        BarClass(){
+            foo=new FooClass(std::bind(&BarClass::Bar_callback_0,this), std::bind(&BarClass::Bar_callback_1,this,std::placeholders::_1));
+        }
+        void Bar_doSomthing(){
+            foo->Foo_do_Somthing();
+        }
+protected:        
+    void Bar_callback_0(){
+        std::cout << "this is a callback method with zero arg \n";
+    } 
+    
+    void Bar_callback_1(int num){
+        
+        std::cout << "this is a callback method with one arg : " << num << "\n";
+    }  
+    private:
+        FooClass *foo;
+    
+};
+
+
 int main()
 {
-    cout<<"Hello World";
-    std::function<void()> c;
-    
-
+    std::cout<<"CallBack Sample\n";
+    BarClass bar;
+    bar.Bar_doSomthing();
     return 0;
 }
