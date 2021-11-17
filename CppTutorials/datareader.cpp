39f301e0096c8b821208c89b89be742def5d1f11
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2021-11-17 12:43:13 +0330

    Update datareader.cpp

--- CppTutorials/datareader.cpp
+++ CppTutorials/datareader.cpp
@@ -6,41 +6,50 @@
 #include <iostream>
 #include <functional>
 
-class InputStream
-{
+class InputStream{
+ 
 public:
     InputStream(){}
-    void read_async(std::size_t count, std::vector<char>& out, 
-                  std::function<void(std::error_code)> callback){
-                      
-                  }
+    void read_async(std::vector<char>& out,std::function<void()> callback){
+        out.push_back('O');
+        out.push_back('v');
+        out.push_back('e');
+        out.push_back('r');
+        out.push_back('-');
+        out.push_back('I');
+        out.push_back('n');
+        out.push_back('f');
+        out.push_back('i');
+        out.push_back('n');
+        out.push_back('i');
+        out.push_back('t');
+        out.push_back('y');
+        
+        callback();                      
+        }
 };
 
-class DataReader
-{
-   /* rest of interface */
+class DataReader{  
+ 
 public:
-  // DataReader(){}
-   void read_remaining()
-   { 
-      stream.read_async(expected - content.size(), content,
-                        std::bind(&DataReader::part_done, this));
+   DataReader(){}   
+   void read_stream(){ 
+      stream.read_async(content,
+                        std::bind(&DataReader::read_done, this));
    }
 
-   void part_done() 
-   {
-     if (content.size() < expected)
-       read_remaining();
+   void read_done() {
+    for (char n : content) {
+        std::cout << n;
+    }
    }
-
    InputStream stream;
-   std::size_t const expected;          
    std::vector<char> content;
 };
 
-int main()
-{
-    std::cout<<"Hello World";
-
+int main(){
+ 
+    DataReader reader;
+    reader.read_stream();    
     return 0;
 }
