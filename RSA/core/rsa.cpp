cc9656cb8cbca230c7e92d7f7816ea88a53dd1a4
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2021-11-23 12:29:05 +0330

    Update rsa.cpp

--- RSA/core/rsa.cpp
+++ RSA/core/rsa.cpp
@@ -25,3 +25,18 @@
  */
 
 /* rsa.cpp */
+/***********************************************************************************
+RSA Algorithm Example:
+
+    Choose p = 3 and q = 11
+    Compute n = p * q = 3 * 11 = 33
+    Compute φ(n) = (p - 1) * (q - 1) = 2 * 10 = 20
+    Choose e such that 1 < e < φ(n) and e and φ (n) are coprime. Let e = 7
+    Compute a value for d such that (d * e) % φ(n) = 1. One solution is d = 3 [(3 * 7) % 20 = 1]
+    Public key is (e, n) => (7, 33)
+    Private key is (d, n) => (3, 33)
+    The encryption of m = 2 is c = 27 % 33 = 29
+    The decryption of c = 29 is m = 293 % 33 = 2
+    
+    https://www.cs.utexas.edu/~mitra/honors/soln.html
+************************************************************************************/
