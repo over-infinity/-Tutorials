b8ab9c3705bf2a5fb07d6e6d89293159d672eeaa
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2022-07-20 18:35:16 +0430

    Create example.cpp

---
+++ DesignPattern/C++/SignalSlot/SingleThread/example.cpp
@@ -0,0 +1,12 @@
+/*
+* @author Over-Infinity
+* @date July 20, 2022
+* @file example.cpp
+* @discription this example shows how to use signal slot in application
+*/
+
+
+int main(){
+ 
+  return 0;
+}
