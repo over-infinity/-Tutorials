fef5e3d43c4739f9b7bc3a151f066bf0b7563c7e
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2022-07-20 18:30:51 +0430

    Update and rename signalslot.h to signalslot_singlethread.h

---
+++ DesignPattern/C++/SignalSlot/signalslot_singlethread.h
@@ -0,0 +1,35 @@
+/* 
+ * Signal Slot Design Pattern
+ * @author Over-Infinity
+ * @data Julay 20, 2022
+ * @file signalslot.h
+ */
+
+#ifndef SIGNAL_SLOT_H
+#define SIGNAL_SLOT_H
+
+namespace utility
+{
+
+  
+template<typename ...Args>
+class signal;
+template<typename ...Args>
+class slot;
+  
+  
+template <typename ...Args>
+class signal
+{
+
+};
+
+  
+template <typename ...Args>
+class slot
+{
+
+};
+  
+}; // end namespace utility
+#endif //SIGNAL_SLOT_H
