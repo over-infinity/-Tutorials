86f34791b85464f008de527a5b14870ca55ed8d2
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2022-11-14 12:19:54 +0330

    Update server.c

--- SocketProgramming/server/server.c
+++ SocketProgramming/server/server.c
@@ -1,1 +1,69 @@
-// server.c
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[256];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     /*********************************************************************************************************************
+      * 1. Socket creation:
+      * int sockfd = socket(domain, type, protocol)
+      * sockfd: socket descriptor, an integer (like a file-handle)
+      * domain: integer, specifies communication domain.
+      * We use AF_ LOCAL as defined in the POSIX standard for communication between processes on the same host.
+      * For communicating between processes on different hosts connected by IPV4, we use AF_INET and
+      * AF_I NET 6 for processes connected by IPV6.
+      * type: communication type
+      * SOCK_STREAM: TCP(reliable, connection oriented)
+      * SOCK_DGRAM: UDP(unreliable, connectionless)
+      * protocol: Protocol value for Internet Protocol(IP), which is 0.
+      * This is the same number which appears on protocol field in the IP header of a packet.(man protocols for more details)
+      ***********************************************************************************************************************/
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
+     printf("Here is the message: %s\n",buffer);
+     n = write(newsockfd,"I got your message",18);
+     if (n < 0) error("ERROR writing to socket");
+     close(newsockfd);
+     close(sockfd);
+     return 0; 
+}
